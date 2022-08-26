e25fa76bde40305c260014a21bd3858b1123f9b0
IDF31 <ardek66@tutanota.com>
2022-08-26 13:04:09 +0300

    Top-level types

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -22,7 +22,7 @@ import XMonad.Hooks.ManageDocks
     ( avoidStruts, docks, manageDocks, ToggleStruts(ToggleStruts) )
 
 import XMonad.Hooks.ManageHelpers ( (-?>), composeOne, isFullscreen, doFullFloat, transience )
-import XMonad.Hooks.WindowSwallowing ( swallowEventHook ) 
+import XMonad.Hooks.WindowSwallowing ( swallowEventHook )
 import XMonad.Hooks.DynamicLog
     ( def,
       dynamicLogWithPP,
@@ -61,33 +61,18 @@ import XMonad.Layout.WindowNavigation (Navigate(Go))
 import XMonad.Actions.Navigation2D (Direction2D(U))
 import System.Environment (getEnv)
 import XMonad.Util.Replace (replace)
+import XMonad.Core ( X, WindowSet, Query)
+import System.IO (Handle)
+import Data.Semigroup (Endo)
+import XMonad.Layout.LayoutModifier (LayoutModifier)
 
+manageHook' :: Query (Endo WindowSet)
 manageHook' =
     composeOne [ transience
                , className =? "QjackCtl" -?> doFloat
                , isFullscreen -?> doFullFloat
                ]
-
-conf' =
-  docks def
-  {
-    terminal = "urxvtc",
-    modMask = mod4Mask, -- optional: use Win key instead of Alt as MODi key
-    focusFollowsMouse = False,
-    borderWidth = 3,
-    normalBorderColor = "#1d2021",
-    focusedBorderColor = "#d79921",
-    workspaces = withScreens 2 $ map show [1..9],
-    manageHook = insertPosition Below Newer <+> manageDocks <+> manageHook',
-    layoutHook = boringWindows . avoidStruts . smartBorders
-                 $ layoutTall
-                 ||| Mirror layoutTall,
-
-    handleEventHook = swallowEventHook (className =? "URxvt") (return True)
-  }
-  where
-    layoutTall = subLayout [] Simplest $ Tall 1 (3/100) (1/2)
-
+barPP' :: PP
 barPP' =
   xmobarPP
   {
@@ -99,6 +84,7 @@ barPP' =
     ppExtras = [ screenLog ]
   }
 
+logHook' :: PP -> Handle -> X ()
 logHook' pp h =
   do
     screenPP 0
@@ -108,16 +94,34 @@ logHook' pp h =
     screenPP screen = (dynamicLogWithPP . marshallCurrentS screen) pp
       { ppOutput = hPutStrLn h }
 
+screenLog :: X (Maybe [Char])
 screenLog =
   withWindowSet $ return . Just . ("\xf878 " ++) . getCurrentScreen
   where getCurrentScreen = show . toInteger . W.screen . W.current
 
+main :: IO ()
 main =
   do
     replace
     xmobar <- getEnv "XMONAD_XMOBAR"
     h <- spawnPipe xmobar
-    conf' <- return conf' { logHook = logHook' barPP' h }
+    let
+      layoutTall = subLayout [] Simplest $ Tall 1 (3/100) (1/2)
+      conf' = docks def
+              { terminal = "urxvtc"
+              , modMask = mod4Mask -- optional: use Win key instead of Alt as MODi key
+              , focusFollowsMouse = False
+              , borderWidth = 3
+              , normalBorderColor = "#1d2021"
+              , focusedBorderColor = "#d79921"
+              , workspaces = withScreens 2 $ map show [1..9]
+              , manageHook = insertPosition Below Newer <+> manageDocks <+> manageHook'
+              , layoutHook = boringWindows . avoidStruts . smartBorders
+                             $ layoutTall
+                             ||| Mirror layoutTall
+              , handleEventHook = swallowEventHook (className =? "URxvt") (return True)
+              , logHook = logHook' barPP' h
+          }
     xmonad . ewmhFullscreen . ewmh $ conf'
       `additionalKeysP` (++)
       [ ("<XF86AudioRaiseVolume>", spawn "amixer sset Master 2%+")
@@ -127,14 +131,14 @@ main =
       , ("S-<Print>", spawn "maim -s | xclip -selection clipboard -t image/png")
       , ("M-q", spawn "xmonad --restart")
       , ("M-p", spawn "rofi -show run")
+      , ("M-c", spawn "rofi -modi \"clipboard:greenclip print\" -show clipboard -run-command '{cmd}'")
       , ("M-z", spawn "emacsclient -c")
-      , ("M-S-z", spawn "emacsclient -s xmonad -c ~/dotfiles/xmonad.hs")
-      , ("M-b", sendMessage ToggleStruts)    
+      , ("M-b", sendMessage ToggleStruts)
       , ("M-C-n", withFocused (sendMessage . UnMerge))
       , ("M-C-S-j", withFocused (sendMessage . mergeDir W.focusDown'))
       , ("M-C-S-k", withFocused (sendMessage . mergeDir W.focusUp'))
       , ("M-C-j", onGroup W.focusDown')
-      , ("M-C-k", onGroup W.focusUp') 
+      , ("M-C-k", onGroup W.focusUp')
       , ("M-j", focusDown)
       , ("M-k", focusUp)
 
